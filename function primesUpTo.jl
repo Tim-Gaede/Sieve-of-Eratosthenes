cea73a69dc96c7b7ff2802b5118a3f26b9e64a72
Tim-Gaede <TimGaede@gmail.com>
2019-08-22 01:13:52 -0400

    Update function primesUpTo.jl

--- function primesUpTo.jl
+++ function primesUpTo.jl
@@ -1,24 +1,47 @@
 #-------------------------------------------------------------------------------
 function primesUpTo(limit::Integer)
-    if limit < 2    return []  end
+    if limit < 2;    return []; end
     primes = Int64[2]
-    πLim = convert( Int64, floor( limit / log(limit) ) )
-    sizehint!(primes, πLim)
-    oddsElim = falses((limit-1) ÷ 2) # odds except 1
-    i_max = (convert(Int64, floor(√limit))-1) ÷ 2
+
+    # Use the prime counting function to set
+    # the amount of memory to allocate.
+    sizehint!(primes, convert( Int64, floor( limit / log(limit) ) ))
+
+    # Keep track of all odd numbers greater than 1
+    # that are still candidates for being prime.
+    # oddsCandidates[index] represents the number, 2index + 1
+    oddsCandidates = trues((limit-1) ÷ 2)
+
+    # Iterate through odd numbers from 3 to the square root of the limit.
+    # If that number is still a viable candidate then it must be prime
+    # and its multiples must be eliminated as being prime.
+    i_max = (convert( Int64, floor(√limit) ) - 1) ÷ 2
     for i = 1 : i_max
-        if !oddsElim[i]
+        if oddsCandidates[i]
             n = 2i + 1
-            for ĩ = i + n : n : length(oddsElim)
-                oddsElim[ĩ] = true
+            for j = i+n : n : length(oddsCandidates)
+                oddsCandidates[j] = false
             end
         end
     end
 
-    for i = 1 : length(oddsElim)
-        if !oddsElim[i]    push!(primes, 2i + 1)  end
+    # Any odd number now that is still a candidate is prime.
+    for i = 1 : length(oddsCandidates)
+        if oddsCandidates[i];    push!(primes, 2i + 1); end
     end
 
     primes
 end
 #-------------------------------------------------------------------------------
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function main()
+    println("\n"^2)
+    @time primes = primesUpTo(10^8)
+    println(typeof(primes))
+end
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+main()
