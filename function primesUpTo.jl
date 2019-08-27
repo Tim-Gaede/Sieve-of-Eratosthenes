d5084777ca2016510b8c9379c91eb4ebd4906b14
Tim-Gaede <TimGaede@gmail.com>
2019-08-27 10:52:45 -0400

    Update function primesUpTo.jl

--- function primesUpTo.jl
+++ function primesUpTo.jl
@@ -5,15 +5,15 @@ function primesUpTo(limit::Integer)
     sizehint!(primes, convert( Int64, floor( limit / log(limit) ) ))
     oddsAlive = trues((limit-1) ÷ 2) # oddsAlive[i] represents 2i + 1
 
-    i_max = (convert( Int64, floor(√limit) ) - 1) ÷ 2
-    for i = 1 : i_max
+    i_sqrt = (convert( Int64, floor(√limit) ) - 1) ÷ 2
+    for i = 1 : i_sqrt
         if oddsAlive[i] # It's prime.  Kill odd multiples of it 
             push!(primes, 2i + 1)
             Δᵢ = 2i + 1
             for iₓ = i+Δᵢ : Δᵢ : length(oddsAlive);   oddsAlive[iₓ] = false; end
         end
     end
-    for i = i_max + 1 : length(oddsAlive) # Remaining living odds also prime
+    for i = i_sqrt + 1 : length(oddsAlive) # Remaining living odds also prime
         if oddsAlive[i];    push!(primes, 2i + 1); end
     end
 
