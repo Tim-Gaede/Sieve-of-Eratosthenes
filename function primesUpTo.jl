f9dc17c954333e6d4ba23a28bdadd0625e7cf216
Tim-Gaede <TimGaede@gmail.com>
2019-08-27 10:45:57 -0400

    Update function primesUpTo.jl

--- function primesUpTo.jl
+++ function primesUpTo.jl
@@ -2,34 +2,19 @@
 function primesUpTo(limit::Integer)
     if limit < 2;    return []; end
     primes = Int64[2]
-
-    # To possibly speed up the function, use the prime counting function
-    # to set the amount of memory to allocate.
     sizehint!(primes, convert( Int64, floor( limit / log(limit) ) ))
+    oddsAlive = trues((limit-1) ÷ 2) # oddsAlive[i] represents 2i + 1
 
-    # Keep track of all odd numbers greater than 1
-    # that are still candidates for being prime.
-    # oddsCandidates[index] represents the number, 2index + 1
-    oddsCandidates = trues((limit-1) ÷ 2)
-
-    # Iterate through the odd candidates up to the square root of the limit.
-    # If that number is still a viable candidate then it must be prime
-    # and its multiples must be eliminated as being prime.
     i_max = (convert( Int64, floor(√limit) ) - 1) ÷ 2
     for i = 1 : i_max
-        if oddsCandidates[i]
-            # The jumps in the index number must be an odd multiple
-            # of the index of the newly discovered prime
-            o = 2i + 1
-            for j = i+o : o : length(oddsCandidates)
-                oddsCandidates[j] = false
-            end
+        if oddsAlive[i] # It's prime.  Kill odd multiples of it 
+            push!(primes, 2i + 1)
+            Δᵢ = 2i + 1
+            for iₓ = i+Δᵢ : Δᵢ : length(oddsAlive);   oddsAlive[iₓ] = false; end
         end
     end
-
-    # Any odd number now that is still a candidate is prime.
-    for i = 1 : length(oddsCandidates)
-        if oddsCandidates[i];    push!(primes, 2i + 1); end
+    for i = i_max + 1 : length(oddsAlive) # Remaining living odds also prime
+        if oddsAlive[i];    push!(primes, 2i + 1); end
     end
 
     primes
